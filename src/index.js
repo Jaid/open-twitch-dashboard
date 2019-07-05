cbe7df14c221d2c18e8459c62c0fef58b6240b41
Jaid <jaid.jsx@gmail.com>
2019-07-05 14:46:52 +0200

    feat: Added parameter `--kiosk`
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 import open from "open"
+import yargs from "yargs"
+import execa from "execa"
 
-open("https://twitch.tv/dashboard")
+const url = "https://twitch.tv/dashboard"
+
+const job = ({kiosk}) => {
+  if (kiosk) {
+    execa("chrome", ["--app", url], {
+      cleanup: false,
+    })
+  } else {
+    open(url)
+  }
+}
+
+const builder = {
+  kiosk: {
+    type: "boolean",
+    default: false,
+    description: "Opens Chrome in kiosk mode",
+  },
+}
+
+yargs
+  .scriptName(_PKG_NAME)
+  .version(_PKG_VERSION)
+  .command("$0", "Opens twitch.tv/dashboard in a browser", builder, job).argv
