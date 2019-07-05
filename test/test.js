6092a90dbf284eaa162c64bb91a3fd6a83225fb4
Jaid <jaid.jsx@gmail.com>
2019-07-05 14:27:43 +0200

    Added test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,1 +1,10 @@
-test.todo("No test needed yet")
+import path from "path"
+
+import coffee from "coffee"
+
+const main = path.resolve(process.env.MAIN)
+
+it("should run", () => coffee.fork(main)
+  .expect("code", 0)
+  .debug(true)
+  .end())
